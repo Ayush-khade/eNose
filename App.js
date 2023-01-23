aac96215631a73fcf23551f61ad5db1c8cb84cf1
Ayush Khade <ayush.khade@cognitiveclouds.com>
2023-01-23 23:42:53 +0530

    AsyncStorage Implementation

--- App.js
+++ App.js
@@ -4,11 +4,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/Login';
 import Register from './screens/Register';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   //This is Hemant
+
+  React.useEffect(()=>{
+    
+
+  },[])
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
