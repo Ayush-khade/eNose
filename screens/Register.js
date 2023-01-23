ffe8850f67a6f8d193a46dee343d59405f59e214
pratik_paraskar <pratikparaskar@gmail.com>
2023-01-23 23:49:25 +0530

    register screen

--- screens/Register.js
+++ screens/Register.js
@@ -1,15 +1,79 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Text, View,TextInput, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
-function Register({ navigation }) {
+
+export default class Register extends React.Component {
+  state={
+name:"",
+emailID:"",
+password:"",
+confirm_password:"",
+secure:"false"
+
+}
+  
+  render(){
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Register Screen</Text>
-      <Button
-        title="Go"
-        onPress={() => {console.log("I M Here");}}
-      />
-    </View>
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+Not registered yet?        Register Here    </Text>
+      <TextInput
+        style={styles.input}
+        value={this.state.value}
+        onChangeText={(v)=>{this.setState({name:v})}}      
+        placeholder="Enter Your Name"
+ />
+      <TextInput
+        style={styles.input}
+        value={this.state.value}
+        onChangeText={(v)=>{this.setState({emailID:v})}}      
+        placeholder="Enter Your EmailID"
+ />
+      
+      <TextInput
+        style={styles.input}
+        value={this.state.value}
+        onChangeText={(v)=>{this.setState({password:v})}}      
+        placeholder="Enter Your password"
+        secureTextEntry={this.state.secure}
+
+ />
+
+<TextInput
+        style={styles.input}
+        value={this.state.value}
+        onChangeText={(v)=>{this.setState({confirm_password:v})}}      
+        placeholder="Enter Your confirm password"
+        secureTextEntry={this.state.secure}
+ />
+
+             {this.state.password == this.state.confirm_password ? <Text>matched</Text>: "Password did not match..." }
+
+
+</View>
   );
 }
-export default Register;
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#362287',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor:"#cccccc"
+  },
+});
